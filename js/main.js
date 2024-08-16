e1360a05471077e864be6f333b9470cd6405bef9
Pazim Goyal <pazimgoyal@gmail.com>
2024-08-15 21:48:32 -0400

    better

--- js/main.js
+++ js/main.js
@@ -15,5 +15,42 @@ $(".contact")
     $('.links_name').html("Links you are looking for");
 });
 
+$(".comp")
+.mouseenter(function() {
+    // let comp = work_info[$(this).data("comp")];
+    let name=$(this).find('img').data("title");
+    $('.work_experience_text').html(name)
+});
+$(".comp")
+.mouseleave(function() {
+    $('.work_experience_text').html("Work Experience");
+});
+
+
+$(".certifications_card").find('img')
+.mouseenter(function() {
+    // let comp = work_info[$(this).data("comp")];
+    let name=$(this).data("title");
+    $('.certifications_name').html(name)
+});
+$(".certifications_card").find('img')
+.mouseleave(function() {
+    $('.certifications_name').html("Certifcations");
+});
+
+
+
+
+
+
+
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+document.addEventListener('mousemove', function(e) {
+    var followImg = document.getElementById('follow-img');
+    followImg.style.left = e.pageX + 'px';
+    followImg.style.top = e.pageY + 'px';
+  });
+  
